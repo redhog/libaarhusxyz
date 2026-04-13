c4f843f974a52b2548f2963a3bf07ed6514dd779
Egil Moeller <redhog@redhog.org>
2026-04-13 21:08:13 +0200

    Bugfix for normalization

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setuptools.setup(
     name='libaarhusxyz',
-    version='0.0.39',
+    version='0.0.40',
     description='Parser for the Aarhus Workbench XYZ format for geophysical data',
     long_description="""Parser for the Aarhus Workbench XYZ format for geophysical data""",
     long_description_content_type="text/markdown",
