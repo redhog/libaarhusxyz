139dc114dccda28d8550b5ac4b00bda62694cc06
Egil Moeller <redhog@redhog.org>
2026-05-27 07:04:40 +0200

    Bugfix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 
 setuptools.setup(
     name='libaarhusxyz',
-    version='0.0.43',
+    version='0.0.44',
     description='Parser for the Aarhus Workbench XYZ format for geophysical data',
     long_description="""Parser for the Aarhus Workbench XYZ format for geophysical data""",
     long_description_content_type="text/markdown",
@@ -20,7 +20,7 @@ setuptools.setup(
         "pandas",
         "pyproj",
         "matplotlib",
-        "pygltflib"
+        "pygltflib",
         "msgpack",
         "msgpack-numpy"
     ],
